c788a496ca86310c5a3077a5c3ac651f1b6ee0ef
Yuval Avnery <yuvalav@mellanox.com>
2020-01-16 15:41:43 +0000

    regex/mlx5: Add query regex support function
    Used to check if device supports regex.

--- drivers/regex/mlx5/mlx5_regex.c
+++ drivers/regex/mlx5/mlx5_regex.c
@@ -3,3 +3,15 @@
  */
 
 #include "mlx5_regex.h"
+
+int mlx5_regex_is_supported(struct ibv_context *ibv_ctx)
+{
+	struct mlx5_hca_attr caps;
+	int err;
+
+	err = mlx5_devx_cmd_query_hca_attr(ibv_ctx, &caps);
+	if (err)
+		return 0;
+
+	return caps.regex;
+}
