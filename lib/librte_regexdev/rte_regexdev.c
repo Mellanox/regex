a9adb63b2629e5c819c71036446d6b30d2237848
Ori Kam <orika@mellanox.com>
2020-01-21 15:19:32 +0000

    net/regexdev: add regexdev core functions
    Signed-off-by: Ori Kam <orika@mellanox.com>

--- lib/librte_regexdev/rte_regexdev.c
+++ lib/librte_regexdev/rte_regexdev.c
@@ -3,4 +3,106 @@
  * Copyright(C) 2020 Mellanox International Ltd.
  */
 
-#include <rte_regexdev.h>
+#include <string.h>
+
+#include <rte_spinlock.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_memzone.h>
+#include <rte_string_fns.h>
+
+#include "rte_regexdev.h"
+#include "rte_regexdev_driver.h"
+
+static struct {
+	struct rte_regex_dev_data data[RTE_MAX_REGEXDEV_DEVS];
+} rte_regex_dev_shared_data;
+
+static struct rte_regex_dev rte_regex_devices[RTE_MAX_REGEXDEV_DEVS];
+
+/* spinlock for shared data allocation */
+static rte_spinlock_t rte_regex_shared_data_lock = RTE_SPINLOCK_INITIALIZER;
+
+
+static uint16_t
+regex_dev_find_free_dev(void)
+{
+	unsigned i;
+
+	for (i = 0; i < RTE_MAX_REGEXDEV_DEVS; i++) {
+		if (rte_regex_devices[i].data != NULL &&
+		    rte_regex_devices[i].data->name[0] == '\0')
+			return i;
+	}
+	return RTE_MAX_REGEXDEV_DEVS;
+}
+
+static const struct rte_regex_dev*
+regex_dev_allocated(const char *name)
+{
+	unsigned i;
+
+	for (i = 0; i < RTE_MAX_REGEXDEV_DEVS; i++) {
+		if (rte_regex_devices[i].data != NULL &&
+		    strcmp(rte_regex_devices[i].data->name, name) == 0)
+			return &rte_regex_devices[i];
+	}
+	return NULL;
+}
+
+static struct rte_regex_dev*
+regex_dev_get(uint16_t dev_id)
+{
+	return (&rte_regex_devices[dev_id]);
+}
+
+struct rte_regex_dev *
+rte_regex_dev_register(const char *name)
+{
+	uint16_t dev_id;
+	struct rte_regex_dev *regex_dev = NULL;
+	size_t name_len;
+
+	name_len = strnlen(name, RTE_REGEX_NAME_MAX_LEN);
+	if (name_len == 0) {
+		RTE_REGEXDEV_LOG(ERR, "Zero length regex device name\n");
+		return NULL;
+	}
+	if (name_len >= RTE_REGEX_NAME_MAX_LEN) {
+		RTE_REGEXDEV_LOG(ERR, "Regex device name is too long\n");
+		return NULL;
+	}
+	/* Synchronize dev creation between primary and secondary threads. */
+	rte_spinlock_lock(&rte_regex_shared_data_lock);
+	if (regex_dev_allocated(name) != NULL) {
+		RTE_REGEXDEV_LOG
+			(ERR, "RegEx device with name %s already allocated\n",
+			 name);
+		goto unlock;
+	}
+	dev_id = regex_dev_find_free_dev();
+	if (dev_id == RTE_MAX_REGEXDEV_DEVS) {
+		RTE_REGEXDEV_LOG
+			(ERR, "Reached maximum number of regex devs\n");
+		goto unlock;
+	}
+	regex_dev = regex_dev_get(dev_id);
+	regex_dev->data = &rte_regex_dev_shared_data.data[dev_id];
+	strlcpy(regex_dev->data->name, name, sizeof(regex_dev->data->name));
+	regex_dev->data->dev_id = dev_id;
+unlock:
+	rte_spinlock_unlock(&rte_regex_shared_data_lock);
+	return regex_dev;
+}
+
+int
+rte_regex_dev_release(struct rte_regex_dev *regex_dev)
+{
+	if (regex_dev == NULL)
+		return -EINVAL;
+	rte_spinlock_lock(&rte_regex_shared_data_lock);
+	memset(regex_dev->data, 0, sizeof(struct rte_regex_dev_data));
+	rte_spinlock_unlock(&rte_regex_shared_data_lock);
+	return 0;
+}
+
---
+++ lib/librte_regexdev/rte_regexdev_driver.h
@@ -0,0 +1,54 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2020 Mellanox Corporation
+ */
+
+#ifndef _RTE_REGEXDEV_DRIVER_H_
+#define _RTE_REGEXDEV_DRIVER_H_
+
+/**
+ * @file
+ *
+ * RTE RegEx Device PMD API
+ *
+ * These APIs for the use from regex drivers, user applications shouldn't
+ * use them.
+ *
+ */
+
+#include <rte_regexdev.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @internal
+ * Register a new regexdev slot for a regex device and returns the pointer
+ * to that slot for the driver to use.
+ *
+ * @param name
+ *   Unique identifier name for each regex device.
+ *
+ * @return
+ *   Slot in the rte_regex_devices array for a new device in case of success
+ *   NULL otherwise.
+ */
+struct rte_regex_dev *rte_regex_dev_register(const char *name);
+
+/**
+ * @internal
+ * Release the specified regexdev port.
+ *
+ * @param dev
+ *   Device to be released.
+ *
+ * @return
+ *   0 on success, Negative errno value on error.
+ */
+int rte_regex_dev_release(struct rte_regex_dev *dev);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_REGEXDEV_DRIVER_H_ */
