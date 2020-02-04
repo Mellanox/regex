f270fc7365cc8a79781636940b3690577ad9c2d6
Vu Pham <vuhuong@mellanox.com>
2020-02-04 13:06:14 -0800

    regex/mlx5: Add Memory Region and its management data structures
    Add Memory Region(MR), its cache and control management data
    structures.
    
    Move mlx5_regex_priv to header file and add extra field for
    MRs cache for each regex device.
    
    Signed-off-by: Vu Pham <vuhuong@mellanox.com>

--- drivers/regex/mlx5/mlx5.h
+++ drivers/regex/mlx5/mlx5.h
@@ -6,6 +6,9 @@
 
 #include <infiniband/mlx5dv.h>
 #include <rte_regexdev_driver.h>
+#include <rte_rwlock.h>
+
+#include "mlx5_regex_mr.h"
 
 struct mlx5_database_ctx {
 	uint32_t umem_id;
@@ -28,6 +31,13 @@ struct mlx5_regex_priv {
 	uint32_t eqn;
 	struct mlx5_regex_db *db_desc;
 	int num_db_desc;
+	struct {
+		uint32_t dev_gen; /* Generation number to flush local caches. */
+		rte_rwlock_t rwlock; /* MR Lock. */
+		struct mlx5_mr_btree cache; /* Global MR cache table. */
+		struct mlx5_mr_list mr_list; /* Registered MR list. */
+		struct mlx5_mr_list mr_free_list; /* Freed MR list. */
+	} mr;
 	TAILQ_ENTRY(mlx5_regex_priv) next;
 	struct rte_pci_device *pci_dev;
 };
