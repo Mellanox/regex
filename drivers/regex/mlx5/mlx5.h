2c3e31f2e7e5036ffccf1da25dc9e221d9b26a70
Vu Pham <vuhuong@mellanox.com>
2020-02-04 13:06:14 -0800

    regex/mlx5: Add Memory Region and its management data structures
    Add Memory Region(MR), its cache and control management data
    structures.
    
    Move mlx5_regex_priv to header file and add extra field for
    MRs cache for each regex device.
    
    Signed-off-by: Vu Pham <vuhuong@mellanox.com>

--- drivers/regex/mlx5/mlx5.h
+++ drivers/regex/mlx5/mlx5.h
@@ -1,9 +1,36 @@
 #ifndef __MLX5_H__
 #define __MLX5_H__
 
+#include <rte_regexdev_driver.h>
+#include <rte_rwlock.h>
+
+#include "mlx5_regex_mr.h"
+
 struct mlx5_database_ctx {
 	uint32_t umem_id;
 	uint64_t offset;
+};
+
+struct mlx5_regex_priv {
+	struct rte_regex_dev regex_dev;
+	struct ibv_context *ctx; /* Device context. */
+	struct ibv_pd *pd;
+	uint32_t pdn;
+	uint32_t eqn;
+	struct mlx5dv_devx_uar *uar;
+
+	struct mlx5_regex_db *db_desc;
+	int num_db_desc;
+
+	TAILQ_ENTRY(mlx5_regex_priv) next;
+	struct rte_pci_device *pci_dev;
+	struct {
+		uint32_t dev_gen; /* Generation number to flush local caches. */
+		rte_rwlock_t rwlock; /* MR Lock. */
+		struct mlx5_mr_btree cache; /* Global MR cache table. */
+		struct mlx5_mr_list mr_list; /* Registered MR list. */
+		struct mlx5_mr_list mr_free_list; /* Freed MR list. */
+	} mr;
 };
 
 #endif
---
+++ drivers/regex/mlx5/mlx5_regex_mr.h
@@ -0,0 +1,69 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2018 6WIND S.A.
+ * Copyright 2018 Mellanox Technologies, Ltd
+ */
+
+#ifndef MLX5_REGEX_MR_H_
+#define MLX5_REGEX_MR_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/queue.h>
+
+/* ISO C doesn't support unnamed structs/unions, disabling -pedantic. */
+#ifdef PEDANTIC
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+#include <infiniband/verbs.h>
+#include <infiniband/mlx5dv.h>
+#ifdef PEDANTIC
+#pragma GCC diagnostic error "-Wpedantic"
+#endif
+
+#include <rte_regexdev.h>
+#include <rte_regexdev_core.h>
+#include <rte_rwlock.h>
+#include <rte_bitmap.h>
+
+/* Size of per-queue MR cache array for linear search. */
+#define MLX5_MR_CACHE_N 8
+
+/* Memory Region object. */
+struct mlx5_mr {
+	LIST_ENTRY(mlx5_mr) mr; /**< Pointer to the prev/next entry. */
+	struct ibv_mr *ibv_mr; /* Verbs Memory Region. */
+	const struct rte_memseg_list *msl;
+	int ms_base_idx; /* Start index of msl->memseg_arr[]. */
+	int ms_n; /* Number of memsegs in use. */
+	uint32_t ms_bmp_n; /* Number of bits in memsegs bit-mask. */
+	struct rte_bitmap *ms_bmp; /* Bit-mask of memsegs belonged to MR. */
+};
+
+/* Cache entry for Memory Region. */
+struct mlx5_mr_cache {
+	uintptr_t start; /* Start address of MR. */
+	uintptr_t end; /* End address of MR. */
+	uint32_t lkey; /* rte_cpu_to_be_32(ibv_mr->lkey). */
+} __rte_packed;
+
+/* MR Cache table for Binary search. */
+struct mlx5_mr_btree {
+	uint16_t len; /* Number of entries. */
+	uint16_t size; /* Total number of entries. */
+	int overflow; /* Mark failure of table expansion. */
+	struct mlx5_mr_cache (*table)[];
+} __rte_packed;
+
+/* Per-queue MR control descriptor. */
+struct mlx5_mr_ctrl {
+	uint32_t *dev_gen_ptr; /* Generation number of device to poll. */
+	uint32_t cur_gen; /* Generation number saved to flush caches. */
+	uint16_t mru; /* Index of last hit entry in top-half cache. */
+	uint16_t head; /* Index of the oldest entry in top-half cache. */
+	struct mlx5_mr_cache cache[MLX5_MR_CACHE_N]; /* Cache for top-half. */
+	struct mlx5_mr_btree cache_bh; /* Cache for bottom-half. */
+} __rte_packed;
+
+LIST_HEAD(mlx5_mr_list, mlx5_mr);
+
+#endif /* MLX5_REGEX_MR_H_ */
