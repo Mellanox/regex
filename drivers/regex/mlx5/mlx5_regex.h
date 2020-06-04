45f1069d26af17d2ddb9c71720412544250356ce
Ori Kam <orika@mellanox.com>
2020-06-04 10:00:01 +0000

    regex/mlx5: add configure function
    This commit implements the configure function.
    This function is responsible to configure the RegEx engine.
    
    Signed-off-by: Ori Kam <orika@mellanox.com>

--- drivers/regex/mlx5/mlx5_regex.h
+++ drivers/regex/mlx5/mlx5_regex.h
@@ -5,15 +5,30 @@
 #ifndef MLX5_REGEX_H
 #define MLX5_REGEX_H
 
+struct mlx5_regex_qp_obj {
+	uint32_t nb_desc; /* Number of desc for this object. */
+};
+
+struct mlx5_regex_qp {
+	uint32_t flags; /* QP user flags. */
+	uint32_t nb_desc; /* Total number of desc for thsi qp. */
+	struct mlx5_regex_qp_obj *qp_obj; /* Pointer to qp_obj array. */
+};
+
 struct mlx5_regex_priv {
 	TAILQ_ENTRY(mlx5_regex_priv) next;
 	struct ibv_context *ctx; /* Device context. */
 	struct rte_pci_device *pci_dev;
 	struct rte_regexdev *regexdev; /* Pointer to the RegEx dev. */
+	uint16_t nb_queues; /* Number of queues. */
+	struct mlx5_regex_qp *qps; /* Pointer to the qp array. */
+	uint16_t nb_max_matches; /* Max number of matches. */
 };
 
 /* mlx5_rxp.c */
 int mlx5_regex_info_get(struct rte_regexdev *dev,
 			struct rte_regexdev_info *info);
+int mlx5_regex_configure(struct rte_regexdev *dev,
+			 const struct rte_regexdev_config *cfg);
 
 #endif /* MLX5_REGEX_H */
