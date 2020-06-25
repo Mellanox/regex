7cbfbc5dc6bac36f5edf993ca48c11fe7cde4b68
Yuval Avnery <yuvalav@mellanox.com>
2020-06-25 20:15:59 +0000

    regex/mlx5: add enqueue implementation
    Will look for a free SQ to send the job on.
    doorbell will be given when sq is full, or no more jobs on the burst.
    
    Signed-off-by: Yuval Avnery <yuvalav@mellanox.com>

--- drivers/regex/mlx5/mlx5_regex_fastpath.c
+++ drivers/regex/mlx5/mlx5_regex_fastpath.c
@@ -59,6 +59,123 @@ struct mlx5_regex_job {
 
 
 static MLX5DV_ALWAYS_INLINE
+void mlx5_regex_set_ctrl_seg(void *seg,
+			     uint8_t le, uint16_t subset_id0,
+			     uint16_t subset_id1, uint16_t subset_id2,
+			     uint16_t subset_id3, uint8_t ctrl)
+{
+	DEVX_SET(regexp_mmo_control, seg, le, le);
+	DEVX_SET(regexp_mmo_control, seg, ctrl, ctrl);
+	DEVX_SET(regexp_mmo_control, seg, subset_id_0, subset_id0);
+	DEVX_SET(regexp_mmo_control, seg, subset_id_1, subset_id1);
+	DEVX_SET(regexp_mmo_control, seg, subset_id_2, subset_id2);
+	DEVX_SET(regexp_mmo_control, seg, subset_id_3, subset_id3);
+}
+
+static inline void
+prep_one(struct mlx5_regex_sq *sq, struct rte_regex_ops *op,
+	 struct mlx5_regex_job *job)
+{
+	memcpy(job->input,
+		rte_pktmbuf_mtod(op->mbuf, void *),
+		rte_pktmbuf_data_len(op->mbuf));
+
+	size_t wqe_offset = (sq->pi % sq_size_get(sq)) * MLX5_SEND_WQE_BB;
+	uint8_t *wqe = (uint8_t *)sq->wqe + wqe_offset;
+	int ds = 4; /*  ctrl + meta + input + output */
+
+	mlx5dv_set_ctrl_seg((struct mlx5_wqe_ctrl_seg *)wqe, sq->pi,
+			    MLX5_OPCODE_MMO,
+			    MLX5_OPC_MOD_MMO_REGEX, sq->obj->id,
+			    0, ds, 0, 0);
+
+	mlx5_regex_set_ctrl_seg(wqe+12, 0, op->group_id0, op->group_id1,
+				op->group_id2,
+				op->group_id3, 0);
+
+	struct mlx5_wqe_data_seg *input_seg =
+		(struct mlx5_wqe_data_seg *)(wqe+32);
+	input_seg->byte_count = htobe32(rte_pktmbuf_data_len(op->mbuf));
+
+	job->user_id = op->user_id;
+	sq->db_pi = sq->pi;
+	sq->pi = (sq->pi+1)%MAX_WQE_INDEX;
+}
+
+static inline void
+send_doorbell(struct mlx5dv_devx_uar *uar, struct mlx5_regex_sq *sq)
+{
+	size_t wqe_offset = (sq->db_pi % sq_size_get(sq)) * MLX5_SEND_WQE_BB;
+	uint8_t *wqe = (uint8_t *)sq->wqe + wqe_offset;
+	((struct mlx5_wqe_ctrl_seg *)wqe)->fm_ce_se = MLX5_WQE_CTRL_CQ_UPDATE;
+	uint64_t *doorbell_addr =
+		(uint64_t *)((uint8_t *)uar->base_addr + 0x800);
+	rte_cio_wmb();
+	sq->dbr[MLX5_SND_DBR] = htobe32(sq->db_pi);
+	rte_wmb();
+	*doorbell_addr = *(volatile uint64_t *)wqe;
+	rte_wmb();
+}
+
+static inline int
+can_send(struct mlx5_regex_sq *sq) {
+	return unlikely(sq->ci > sq->pi) ?
+			MAX_WQE_INDEX + sq->pi - sq->ci < sq_size_get(sq) :
+			sq->pi - sq->ci < sq_size_get(sq);
+}
+
+static inline uint32_t
+job_id_get(uint32_t qid, size_t sq_size, size_t index) {
+	return qid*sq_size + index%sq_size;
+}
+
+/**
+ * DPDK callback for enqueue.
+ *
+ * @param dev
+ *   Pointer to the regex dev structure.
+ * @param qp_id
+ *   The queue to enqueue the traffic to.
+ * @param ops
+ *   List of regex ops to enqueue.
+ * @param nb_ops
+ *   Number of ops in ops parameter.
+ *
+ * @return
+ *   Number of packets successfully enqueued (<= pkts_n).
+ */
+uint16_t
+mlx5_regexdev_enqueue(struct rte_regexdev *dev, uint16_t qp_id,
+		      struct rte_regex_ops **ops, uint16_t nb_ops)
+{
+	struct mlx5_regex_priv *priv = dev->data->dev_private;
+	struct mlx5_regex_qp *queue = &priv->qps[qp_id];
+	struct mlx5_regex_sq *sq;
+	size_t sqid, job_id, i = 0;
+
+	while ((sqid = ffs(queue->free_sqs))) {
+		sqid--; /* ffs returns 1 for bit 0 */
+		sq = &queue->sqs[sqid];
+		while (can_send(sq)) {
+			job_id = job_id_get(sqid, sq_size_get(sq), sq->pi);
+			prep_one(sq, ops[i], &queue->jobs[job_id]);
+			i++;
+			if (unlikely(i == nb_ops)) {
+				send_doorbell(priv->uar, sq);
+				goto out;
+			}
+		}
+		queue->free_sqs &= ~(1 << sqid);
+		send_doorbell(priv->uar, sq);
+	}
+
+out:
+	queue->pi += i;
+	return i;
+}
+
+
+static MLX5DV_ALWAYS_INLINE
 void mlx5dv_set_metadata_seg(struct mlx5_wqe_metadata_seg *seg,
 			     uint32_t mmo_control_31_0, uint32_t lkey,
 			     uintptr_t address)
